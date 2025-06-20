b2bdfcf12e584d00a93207156c7ef69f2667aefb
priyapatil2004 <priyapatil0929@gmail.com>
2025-06-20 22:56:01 +0530

    Fixed import issue in user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 const User=require("../models/user.js");
-const wrapasync = require("../utils/wrapasync.js");
+const wrapasync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
